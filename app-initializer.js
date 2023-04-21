5c12f4cd60fe0e71e3d5905d193d07e11e04b2f1
gmccosta <chrishanthacosta@gmail.com>
2023-04-21 21:20:55 +0530

    sec commit- add new project pop

--- app-initializer.js
+++ app-initializer.js
@@ -1,10 +1,33 @@
 import { appDiv } from "./index-febe";
 import { setupInitialLoginPage } from "./src/app-initializers.js/1setup-initial-login-page";
 import {   setupNewOpenSelectionPopup } from './src/app-initializers.js/2setup-new-open-selection';
+import { showNewProjectPopup } from './src/app-initializers.js/3setup-new-project-popup';
+import { Subject } from 'rxjs';
 
 
 
 
 const { loginButton } = setupInitialLoginPage(appDiv);
  
-setupNewOpenSelectionPopup(loginButton,appDiv);
+// const projectSelectionCallBack = (selectionObject) => {
+//   if (selectionObject == "new") {
+//     showNewProjectPopup();
+//   } else {
+//   }
+// };
+
+const projectSelection$ = new Subject();
+setupNewOpenSelectionPopup(loginButton, appDiv, projectSelection$);
+// {type:"new" | "existing", fileName:"path"}
+
+projectSelection$.subscribe({
+    next: (returnValue) => {
+        if (returnValue == "new") {
+            showNewProjectPopup();
+        } else {
+            //showExistingOpen()
+        }
+     }
+});
+
+
--- src/app-initializers.js/2setup-new-open-selection.js
+++ src/app-initializers.js/2setup-new-open-selection.js
@@ -1,16 +1,30 @@
 import { showSearchModalType3 } from "./../ui-widgets/modal-type1/modal-type3-buttons";
+import { showNewProjectPopup } from './3setup-new-project-popup';
 
-export const setupNewOpenSelectionPopup = (loginButton, appDiv) => {
-  console.log("btn", loginButton);
+export const setupNewOpenSelectionPopup = async (
+  loginButton,
+  appDiv,
+  projectSelection$
+) => {
   loginButton.appDiv = appDiv;
+  loginButton.projectSelection$ = projectSelection$;
   loginButton.addEventListener("click", loginButtonHandler);
 };
 
 const loginButtonHandler = async (e) => {
+  let projectSelection$ = e.target.projectSelection$;
+
   const appDiv = e.target.appDiv;
     appDiv.firstElementChild.remove();
     console.log("appDiv", appDiv.firstChild);
-  await showSearchModalType3("Select Project", "", [
+ const retValue =  await showSearchModalType3("Select Project", "", [
     { text: "New Project", returnValue: "new", class: "btn-primary" },
-  ]);
+ ]);
+  
+  projectSelection$.next(retValue); 
+  
+ // callBack(retValue);
+  // if (retValue == "new") {
+  //   showNewProjectPopup();
+  // }
 };
